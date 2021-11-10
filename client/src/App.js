ba3b593684c4e3b7fb406b066d3510371d6e5566
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-11-10 16:01:02 -0700

    completed AddMovieForm, works

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
 
+import AddMovieForm from "./components/AddMovieForm";
 import EditMovieForm from './components/EditMovieForm';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
@@ -47,6 +48,10 @@ const App = (props) => {
           <Switch>
             <Route path="/movies/edit/:id">
               <EditMovieForm setMovies={setMovies}/>
+            </Route>
+
+            <Route path="/movies/add/" component={AddMovieForm}>
+              <AddMovieForm setMovies={setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
