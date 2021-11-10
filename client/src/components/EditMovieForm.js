5f43048231cecda7d608470c220bc4985227efb6
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-11-10 13:42:25 -0700

    added useEffect and handleSubmit code to editmovieform

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 const EditMovieForm = (props) => {
 	const { push } = useHistory();
+	const { id } = useParams();
 
 	const [movie, setMovie] = useState({
 		title:"",
@@ -21,9 +22,27 @@ const EditMovieForm = (props) => {
             [e.target.name]: e.target.value
         });
     }
+	useEffect(() => {
+		axios.get(`http://localhost:5000/api/movies/${id}`)
+		.then(res => {
+			console.log(res.data);
+			setMovie(res.data);
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	}, []);
 
     const handleSubmit = (e) => {
 		e.preventDefault();
+		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+		.then(res => {
+			setMovie(res.data);
+			push(`/movies/${id}`);
+		})
+		.catch(err => {
+			console.log(err);
+		})
 	}
 	
 	const { title, director, genre, metascore, description } = movie;
